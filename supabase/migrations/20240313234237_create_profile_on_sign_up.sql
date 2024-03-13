99f4f88482c1a00471bba17dcd533fe17fdf0474
voiys <matej2714@gmail.com>
2024-03-14 00:46:03 +0100

    add trigger for creating profile on sign up

---
+++ supabase/migrations/20240313234237_create_profile_on_sign_up.sql
@@ -0,0 +1,22 @@
+DROP FUNCTION IF EXISTS public.create_profile_on_sign_up() CASCADE;
+
+DROP TRIGGER IF EXISTS create_profile_on_sign_up ON auth.table_name;
+
+CREATE OR REPLACE FUNCTION public.create_profile_on_sign_up()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    AS $function$
+BEGIN
+    RAISE LOG 'New row: %', row_to_json(NEW);
+    INSERT INTO public.profile(id, username)
+        VALUES(NEW.id, NEW.raw_user_meta_data ->> 'name');
+    RETURN NEW;
+END;
+$function$;
+
+CREATE TRIGGER create_profile_on_sign_up
+    AFTER INSERT ON auth.users
+    FOR EACH ROW
+    EXECUTE FUNCTION public.create_profile_on_sign_up();
+
